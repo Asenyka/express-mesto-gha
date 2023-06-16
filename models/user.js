d8253dd0852e1f15d1e8b84ae14179be2fb6fed5
Asenyka <115739939+Asenyka@users.noreply.github.com>
2023-06-16 21:09:59 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,15 +5,15 @@ const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minLength: 2,
+    maxLength: 30,
 
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minLength: 2,
+    maxLength: 30,
   },
   avatar: {
     type: String,
