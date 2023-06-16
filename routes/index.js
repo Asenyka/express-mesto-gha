047bc550772f38aaf11cb2f88cfbef8b1fb21970
Asenyka <humsterchik@yandex.ru>
2023-06-16 21:03:46 +0300

    8th ed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardsRouter = require('./cards');
-const errorRouter = require('./error');
+
 
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
-router.use('/', errorRouter)
 module.exports = router;
