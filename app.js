daaca770d55d856deec6d1a4bf673372c58b34a7
Asenyka <humsterchik@yandex.ru>
2023-06-18 10:56:29 +0300

    11th ed

--- app.js
+++ app.js
@@ -12,12 +12,13 @@ app.use((req, res, next) => {
 
   next();
 });
-//app.use((req, res, next) => {
- // res.status(404).send({message:"Запрашиваемая страница не найдена"});
- //  next();
-//});
+
 app.use(express.json());
 
 app.use(router);
+app.use((req, res, next) => {
+  res.status(404).send({message:"Запрашиваемая страница не найдена"});
+  next();
+ });
 app.listen(3000, () => {
 });
